fe97131c120cda0ddf507bb9a5daef894a5e7220
Billy Franscois <BillyFranscois@outlook.com>
2022-06-17 13:23:54 +0900

    Update SQLQuery.sql

--- SmarterBooks/wwwroot/sql/SQLQuery.sql
+++ SmarterBooks/wwwroot/sql/SQLQuery.sql
@@ -1,7 +1,9 @@
-CREATE DATABASE SmarterBooks;
+CREATE DATABASE [SmarterBooks];
 
 SET IDENTITY_INSERT Books OFF;
 
-INSERT INTO Books(Name, Author) VALUES ('Design Pattern', 'Sendy Friscilla');
+INSERT INTO [Books](Name, Author) VALUES ('Design Pattern', 'Sendy Friscilla');
 
-SELECT * FROM Books;
+SELECT * FROM [Books];
+
+DROP TABLE [Books];
