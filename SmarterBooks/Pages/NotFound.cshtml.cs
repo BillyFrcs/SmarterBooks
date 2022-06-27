96def4ed60c26f5f3a6275ac639498770cdec225
Billy Franscois <BillyFranscois@outlook.com>
2022-06-27 14:00:23 +0900

    Handle 404 error

---
+++ SmarterBooks/Pages/NotFound.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmarterBooks.Pages
+{
+    public class NotFoundModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
