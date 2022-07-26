2efbffdaaf9a2fbedccf12a9f0491f25a3f0bc4d
Billy Franscois <BillyFranscois@outlook.com>
2022-07-26 13:24:17 +0900

    Update Command Migrations

---
+++ Commands/Migrations.sh
@@ -0,0 +1,13 @@
+# Visual Studio:
+# Run Database Migration
+add-migration MigrationName
+
+# Update Database Migration
+update-database
+
+# Visual Studio Code:
+# Run Database Migration: 
+dotnet ef migrations add MigrationName --project ProjectName
+
+# Update Database Migration
+dotnet ef database update --project ProjectName
